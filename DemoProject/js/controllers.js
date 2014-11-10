f721149054cd63be737c03f7066171d363311376
subodhbrahmi <subodh_brahmi@yahoo.co.in>
2014-11-10 07:39:49 -0600

    Adding index.html and controllers.js from lynda.com tutorial

---
+++ DemoProject/js/controllers.js
@@ -0,0 +1,11 @@
+﻿
+var lyndaTutnamespace = angular.module('myApp', []);
+
+lyndaTutnamespace.controller('MyController', function MyController($scope) { //// CONTROLLER ////
+    /******* MODEL *******/
+    $scope.author = {  // Variable Scope and authoer with javascript objects (name, title and company)
+        'name': 'Subodh',
+        'title': 'Staff Author',
+        'company': 'Spawning Designs'
+    }
+});
